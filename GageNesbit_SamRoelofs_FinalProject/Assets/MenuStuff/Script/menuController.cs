abd31a1a7f3f436dae747bb3bbded5746a91c826
gagenesbit <gagenesbit31@gmail.com>
2023-12-06 15:55:44 -0700

    Saving Mechanic Done

--- GageNesbit_SamRoelofs_FinalProject/Assets/MenuStuff/Script/menuController.cs
+++ GageNesbit_SamRoelofs_FinalProject/Assets/MenuStuff/Script/menuController.cs
@@ -1,67 +1,75 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using TMPro;
-using System.Runtime.Serialization.Formatters.Binary;
-using System.IO;
-using System;
 
 public class MainMenuController : MonoBehaviour
 {
-    public GameObject mainMenu,settings, credits, levels, audio;
+    public GameObject mainMenu, settings, credits, levels, audio;
     public GameObject levelTwoButton;
-    private bool level1Completed=false;
-
     void Start()
     {
-
         mainOpen();
     }
-    
-    public void playOne(){
+
+    public void playOne()
+    {
+        //levelTwoButton.SetActive(true);
+        //PlayerPrefs.SetInt("twoButtonOn", levelTwoButton.activeSelf ? 1 : 0);
+        //PlayerPrefs.Save();
+        //levelTwoButton.SetActive(false);
         SceneManager.LoadScene("LevelOne");
     }
-    public void playTwo(){
+
+    public void playTwo()
+    {
         SceneManager.LoadScene("LevelTwo");
     }
 
-    public void musicToggle(){
+    public void musicToggle()
+    {
         Debug.Log("Music Toggle");
-        if(audio.activeSelf){
-            audio.SetActive(false);
-        }
-        else{
-            audio.SetActive(true);
-        }
+        audio.SetActive(!audio.activeSelf);
     }
 
-
-    public void mainOpen(){
+    public void mainOpen()
+    {
         turnAllOff();
         mainMenu.SetActive(true);
+        levelTwoButton.SetActive(PlayerPrefs.GetInt("twoButtonOn", 0) == 1);;
     }
 
-    public void levelSelectOpen(){
+    public void levelSelectOpen()
+    {
         turnAllOff();
         levels.SetActive(true);
-
     }
 
-    public void settingsOpen(){
+    public void settingsOpen()
+    {
         turnAllOff();
         settings.SetActive(true);
     }
 
-    public void creditsOpen(){
+    public void creditsOpen()
+    {
         turnAllOff();
         credits.SetActive(true);
     }
 
-    private void turnAllOff(){
+    private void turnAllOff()
+    {
         credits.SetActive(false);
         settings.SetActive(false);
-        settings.SetActive(false);
         mainMenu.SetActive(false);
+        levels.SetActive(false);
     }
 
+    public void reset()
+    {
+        levelTwoButton.SetActive(false);
+        PlayerPrefs.SetInt("twoButtonOn", levelTwoButton.activeSelf ? 1 : 0);
+        PlayerPrefs.Save();
+    }
+
+    
 }
